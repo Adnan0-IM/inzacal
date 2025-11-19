1a171f725a316d93b30759a65af9cf21ad089563
Adnan0-IM <adnanilyas969@gmail.com>
2025-11-19 08:38:49 +0100

    feat: enhance useOrganization hook to include loading states for active organization and organizations list

--- client/src/features/dashboard/hooks/useOrganization.ts
+++ client/src/features/dashboard/hooks/useOrganization.ts
@@ -4,17 +4,17 @@ import { AuthUIContext } from "@daveyplate/better-auth-ui";
 export function useOrganization() {
   // Get active organization
   const { hooks } = useContext(AuthUIContext);
-  const { data: activeOrg } = hooks.useActiveOrganization();
+  const { data: activeOrg, isPending: isActiveOrgPending } = hooks.useActiveOrganization();
 
   // List all organizations
-  const { data: organizations } = hooks.useListOrganizations();
+  const { data: organizations, isPending: isOrganizationsPending } = hooks.useListOrganizations();
 
   // Check permissions
-  const { data: hasPermission } = hooks.useHasPermission({
-    permissions: {
-      organization: ["update"],
-      member: ["create", "delete"],
-    },
-  });
-  return { activeOrg, organizations, hasPermission };
+  // const { data: hasPermission, isPending: isHasPermissionPending } = hooks.useHasPermission({
+  //   permissions: {
+  //     organization: ["update"],
+  //     member: ["create", "delete"],
+  //   },
+  // });
+  return { activeOrg, isActiveOrgPending, organizations, isOrganizationsPending };
 }
