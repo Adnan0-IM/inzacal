4222ee1811cca3f5f8fa0342ee8cf948c218c8b1
Adnan0-IM <adnanilyas969@gmail.com>
2025-11-19 14:01:54 +0100

    feat: update SERVER_URL to use production URL and comment out local development fallback

--- client/src/config/constants.ts
+++ client/src/config/constants.ts
@@ -1,5 +1,6 @@
 export const SERVER_URL =
-  (import.meta.env.VITE_SERVER_URL as string) || "http://localhost:3000";
+  // (import.meta.env.VITE_SERVER_URL as string) || "http://localhost:3000";
+  (import.meta.env.VITE_SERVER_URL as string) || "https://inzacal-production.up.railway.app/";
 
 // API base points to the server’s /api namespac
 export const API_BASE_URL = `${SERVER_URL}/api`;
