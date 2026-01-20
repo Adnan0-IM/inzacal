01acbca6287d01387dc95934e2f07075cbd916c9
Adnan0-IM <adnanilyas969@gmail.com>
2026-01-20 16:34:53 +0100

    feat: update SERVER_URL and API_BASE_URL configuration for production

--- client/src/config/constants.ts
+++ client/src/config/constants.ts
@@ -1,9 +1,6 @@
-// export const SERVER_URL =
+export const SERVER_URL =
   // (import.meta.env.VITE_SERVER_URL as string) || "http://localhost:3000";
-  // (import.meta.env.VITE_SERVER_URL as string) ||  window.location.origin;
+  (import.meta.env.VITE_SERVER_URL as string) ||  window.location.origin;
 
 // API base points to the server’s /api namespac
-// export const API_BASE_URL = `${SERVER_URL}/api`;
-export const API_BASE_URL = import.meta.env.PROD
-  ? "/api"
-  : "http://localhost:3000/api";
+export const API_BASE_URL = `${SERVER_URL}/api`;
